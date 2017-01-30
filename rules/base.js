51d37161175c9b609c73e75ad49af0166b70f33e
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2017-01-30 19:51:06 +0300

    add a comment about what rules go where

--- rules/base.js
+++ rules/base.js
@@ -1,8 +1,14 @@
+/**
+ * Правила для работы с чистым JS,
+ * вне зависимости от используемых фреймворков
+ * или сахара вроде JSX.
+ */
 module.exports = {
   extends: [
     'eslint-config-airbnb-base',
     'eslint-config-airbnb-base/rules/strict',
   ].map(require.resolve),
+  // дополнения/переопределения правил от airbnb
   rules: {
     // аргументы стрелочных функций всегда заворачиваем в скобчки
     // http://eslint.org/docs/rules/arrow-parens
